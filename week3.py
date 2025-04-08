26e3406a8fca26695e8e6781dd4ef9498a87dbf9
Stephen <steph.okoth28@gmail.com>
2025-04-08 07:16:05 -0500

    Final code

---
+++ week3.py
@@ -0,0 +1,17 @@
+# Defining function
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        selling_price = price * (1 - (discount_percent/100))
+        return selling_price
+    else:
+        return price
+
+#Prompting user and calling function 
+price= int(input("Please enter the original price: "))
+discount_percent =int(input("Please enter the percent discount: "))
+selling_price = calculate_discount(price, discount_percent)
+if selling_price < price:
+    print("The selling price after applying discount is: ", selling_price)
+else:
+    print("There is no discount applied, the selling_price is:", price)
+
